e47e77ff2972456e6dbb5ccbe80c9e4b37979e28
Jeff <jeff@hermex.dev>
2026-07-16 23:21:55 +0000

    align versionName to 2.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 34
         targetSdk = 34
         versionCode = 309
-        versionName = "2.2.1"
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
