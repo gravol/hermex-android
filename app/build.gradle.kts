22a3c0a43529c7808296835eb810e05d3529050a
Jeff <jeff@hermex.dev>
2026-07-17 15:42:33 +0000

    v0.1.17: bump version after Phase 4D debug instrumentation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hermex.android"
         minSdk = 34
         targetSdk = 34
-        versionCode = 16
-        versionName = "0.1.16"
+        versionCode = 17
+        versionName = "0.1.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
