4a8af3b4244db2159512811571d057f6da605221
Jeff <jeff@hermex.dev>
2026-08-18 22:17:06 +0000

    Bump version to 0.1.112/112 for tok/s fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hermex.android"
         minSdk = 34
         targetSdk = 34
-        versionCode = 111
-        versionName = "0.1.111"
+        versionCode = 112
+        versionName = "0.1.112"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
