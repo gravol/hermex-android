1141dbe40124507652e8839e7f21486afb620c49
Jeff <jeff@hermex.dev>
2026-09-17 13:16:00 +0000

    v0.1.165: build from HEAD with 4007 self-heal retry budget (resumeUntilLive), versionCode 166

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hermex.android"
         minSdk = 34
         targetSdk = 34
-        versionCode = 165
-        versionName = "0.1.164"
+        versionCode = 166
+        versionName = "0.1.165"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
